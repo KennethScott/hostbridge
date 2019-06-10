8b8c25a5df477f66059a5b6f245e9697089d8bcd
Scott, Kenneth <kenneth.scott@bxs.com>
2019-06-10 13:11:25 -0500

    refactored code and added output window logging

--- src/extension.ts
+++ src/extension.ts
@@ -1,71 +1,82 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as request from 'request-promise-native';
 import * as fs from 'fs';
 import { stringify } from 'querystring';
 import { debug } from 'util';
 import { UriOptions } from 'request';
+import { OutputChannel } from 'vscode';
+
+
+class HostbridgeFile {
+	public filename: string;
+	public folder: string;
+	public contents: string;
+
+	constructor(uri: vscode.Uri) {
+		let uriPieces = uri.fsPath.split("\\");
+		this.filename = uriPieces[uriPieces.length-1];
+		this.folder = uriPieces[uriPieces.length-2];	
+		// no idea why this doesn't seem to work..
+		// vscode.workspace.openTextDocument(uri).then((document) => {
+		// 	fileContents = document.getText();
+		// });
+		this.contents = fs.readFileSync(uri.fsPath, 'utf-8');
+	}
+
+	/// action = MAKE or RUN
+	getHttpOptions(action:string): UriOptions {
+
+		let config = vscode.workspace.getConfiguration('hostbridge');
+			
+		let x =  
+		{				
+			method: 'POST',
+			uri: "https://" + config.host + ":" + config.port + "/" + this.folder + "/mscript",
+			//uri: "https://" + config.host + ":" + config.port + "/" + '***REMOVED***' + "/mscript",
+			headers: {
+				'Authorization': "Basic " + Buffer.from(config.userid + ':' + 'nowayjose').toString('base64'),
+				'X-HB-ACTION': action,
+				'X-HB-ACTION-TARGET': this.filename,
+				'X-HB-TRANSLATE': 'text',
+				'Content-Type': 'text/plain',
+				'Cache-Control': 'no-cache',
+				'Pragma': 'no-cache'				
+			},
+			body: this.contents
+		};
+
+		return x;
+	}		
+}
+
+
+let _channel: vscode.OutputChannel;
+function getOutputChannel(): vscode.OutputChannel {
+	if (!_channel) {
+		_channel = vscode.window.createOutputChannel('Hostbridge');
+	}
+	return _channel;
+}
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	class HostbridgeFile {
-		public filename: string;
-		public folder: string;
-		public contents: string;
-
-		constructor(uri: vscode.Uri) {
-			let uriPieces = uri.fsPath.split("\\");
-			this.filename = uriPieces[uriPieces.length-1];
-			this.folder = uriPieces[uriPieces.length-2];	
-			// no idea why this doesn't seem to work..
-			// vscode.workspace.openTextDocument(uri).then((document) => {
-			// 	fileContents = document.getText();
-			// });
-			this.contents = fs.readFileSync(uri.fsPath, 'utf-8');
-		}
-
-		/// action = MAKE or RUN
-		getHttpOptions(action:string): UriOptions {
-
-			let config = vscode.workspace.getConfiguration('hostbridge');
-				
-			let x =  
-			{				
-				method: 'POST',
-				uri: "https://" + config.host + ":" + config.port + "/" + this.folder + "/mscript",
-				//uri: "https://" + config.host + ":" + config.port + "/" + '***REMOVED***' + "/mscript",
-				headers: {
-					'Authorization': "Basic " + Buffer.from(config.userid + ':' + 'nowayjose').toString('base64'),
-					'X-HB-ACTION': action,
-					'X-HB-ACTION-TARGET': this.filename,
-					'X-HB-TRANSLATE': 'text',
-					'Content-Type': 'text/plain',
-					'Cache-Control': 'no-cache',
-					'Pragma': 'no-cache'				
-				},
-				body: this.contents
-			};
-
-			return x;
-		}		
-	}
+export function activate(context: vscode.ExtensionContext) {	
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hostbridge" is now active!');
+	console.log('Hostbridge extension active.');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable2 = vscode.commands.registerCommand('extension.make', async (uri:vscode.Uri) => {
-		// The code you place here will be executed every time your command is executed		
+	let disposableMake = vscode.commands.registerCommand('extension.make', async (uri:vscode.Uri) => {
+	
 		let response: any = {};
 
 		let hbFile = new HostbridgeFile(uri);
 		
+		//#region MAKE Headers 
 		// POST /***REMOVED***/mscript HTTP/1.1
 		// X-HB-ACTION: MAKE
 		// X-HB-ACTION-TARGET: Test3.hbx
@@ -81,29 +92,30 @@ export function activate(context: vscode.ExtensionContext) {
 		// Accept: text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2
 		// Connection: keep-alive
 		// Content-Length: 994
+		//#endregion
 
 		let options = hbFile.getHttpOptions("MAKE");
 
-		// Here we go!
 		const result = await request.post(options)
 		 	.then((body) => { response = body; })
-		 	.catch ((err) => { response = { "origin": err.toString() }; });
+		 	.catch ((err) => { response = err; });
 		
-		// Now that we have our response, pull out the origin and display it
-		// Display a message box to the user
 		console.log(response);
-		vscode.window.showInformationMessage(response);
+		//vscode.window.showInformationMessage(response);
+		getOutputChannel().appendLine(response);
+		getOutputChannel().show(true);		
 	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable3 = vscode.commands.registerCommand('extension.exec', async (uri:vscode.Uri) => {
+	let disposableExec = vscode.commands.registerCommand('extension.exec', async (uri:vscode.Uri) => {
 
 		let response: any = {};
 
 		let hbFile = new HostbridgeFile(uri);
 
+		//#region RUN (Exec) Headers 
 		// POST /***REMOVED***/mscript HTTP/1.1
 		// X-HB-ACTION: RUN
 		// X-HB-ACTION-TARGET: Test3.hbx
@@ -119,21 +131,21 @@ export function activate(context: vscode.ExtensionContext) {
 		// Accept: text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2
 		// Connection: keep-alive
 		// Content-Length: 979
+		//#endregion
 
 		let options = hbFile.getHttpOptions("RUN");
 
-		// Here we go!
 		const result = await request.post(options)
 		 	.then((body) => { response = body; })
-		 	.catch ((err) => { response = { "origin": err.toString() }; });
+		 	.catch ((err) => { response = err; });
 		
-		// Now that we have our response, pull out the origin and display it
-		// Display a message box to the user
 		console.log(response);		
-		vscode.window.showInformationMessage(response);
+		//vscode.window.showInformationMessage(response);
+		getOutputChannel().appendLine(response);
+		getOutputChannel().show(true);
 	});	
 
-	context.subscriptions.push(disposable2, disposable3);
+	context.subscriptions.push(disposableMake, disposableExec);
 }
 
 // this method is called when your extension is deactivated
