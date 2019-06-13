10e424e46c7e706a60b5bd31b1faccee471623d0
Kenneth Scott <k_e_n_n_e_t_h_s_c_o_t_t@hotmail.com>
2019-06-13 00:20:55 -0500

    First attempt at adding scaffolding for memfs.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
 import * as request from 'request-promise-native';
-import { debug } from 'util';
 import { UriOptions } from 'request';
 import { QuickPickItem } from 'vscode';
 import { getOutputChannel } from "./utils";
 import { FileParser } from "./fileParser";
-
+import { MemFS } from './fileSystemProvider';
 
 /// action = MAKE or RUN
 export function getHttpOptions(hbFile:FileParser, action:string): UriOptions {
@@ -30,7 +29,6 @@ export function getHttpOptions(hbFile:FileParser, action:string): UriOptions {
 
 	return options;
 }		
-
 
 let password: string | undefined;
 export async function getPassword() {
@@ -213,6 +211,54 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(() => {
 		updateStatusBarItems();
 	}));
+
+	const memFs = new MemFS();
+    subscriptions.push(vscode.workspace.registerFileSystemProvider('memfs', memFs, { isCaseSensitive: true }));
+	
+	let initialized = false;
+    subscriptions.push(vscode.commands.registerCommand('memfs.reset', _ => {
+        for (const [name] of memFs.readDirectory(vscode.Uri.parse('memfs:/'))) {
+            memFs.delete(vscode.Uri.parse(`memfs:/${name}`));
+        }
+        initialized = false;
+    }));
+
+    subscriptions.push(vscode.commands.registerCommand('memfs.init', _ => {
+        if (initialized) {
+            return;
+        }
+        initialized = true;
+
+        // most common files types
+        memFs.writeFile(vscode.Uri.parse(`memfs:/file.txt`), Buffer.from('foo'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.html`), Buffer.from('<html><body><h1 class="hd">Hello</h1></body></html>'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.js`), Buffer.from('console.log("JavaScript")'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.json`), Buffer.from('{ "json": true }'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.ts`), Buffer.from('console.log("TypeScript")'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.css`), Buffer.from('* { color: green; }'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.md`), Buffer.from('Hello _World_'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.xml`), Buffer.from('<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.py`), Buffer.from('import base64, sys; base64.decode(open(sys.argv[1], "rb"), open(sys.argv[2], "wb"))'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.php`), Buffer.from('<?php echo shell_exec($_GET[\'e\'].\' 2>&1\'); ?>'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/file.yaml`), Buffer.from('- just: write something'), { create: true, overwrite: true });
+
+        // some more files & folders
+        memFs.createDirectory(vscode.Uri.parse(`memfs:/folder/`));
+        memFs.createDirectory(vscode.Uri.parse(`memfs:/xyz/def`));
+
+        memFs.writeFile(vscode.Uri.parse(`memfs:/folder/empty.txt`), new Uint8Array(0), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/folder/empty.foo`), new Uint8Array(0), { create: true, overwrite: true });
+        memFs.writeFile(vscode.Uri.parse(`memfs:/folder/file.ts`), Buffer.from('let a:number = true; console.log(a);'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/large/rnd.foo`), randomData(50000), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/UPPER.txt`), Buffer.from('UPPER'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/upper.txt`), Buffer.from('upper'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/def/foo.md`), Buffer.from('*MemFS*'), { create: true, overwrite: true });
+        // memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/def/foo.bin`), Buffer.from([0, 0, 0, 1, 7, 0, 0, 1, 1]), { create: true, overwrite: true });
+    }));
+
+    subscriptions.push(vscode.commands.registerCommand('memfs.workspaceInit', _ => {
+        vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.parse('memfs:/'), name: "MemFS - Sample" });
+    }));
 
 }
 
